3d946613c0a2965fc89528616d81f7b6d0328c3e
Jessica Sarlin <jessica.sarlin@gmail.com>
2019-05-02 20:14:47 +0300

    Spring clean up - unnecessary files removed

--- FriendFace_Social_Media_Project/wepa_Projekti/src/main/java/projekti/PictureAlbumRepository.java
+++
@@ -1,9 +0,0 @@
-package projekti;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PictureAlbumRepository extends JpaRepository<PictureAlbum, Long> {
-
-    PictureAlbum getPictureAlbumByOwner(UserAccount owner);
-
-}
